1acadff94cda8c6381759020afa7f9c0b211d3fb
lydiandy <lydiandylin@gmail.com>
2020-12-01 21:45:09 +0800

    update to new V

--- vast.v
+++ vast.v
@@ -1486,6 +1486,11 @@ fn (t Tree) array_init(node ast.ArrayInit) &C.cJSON {
 	to_object(obj, 'has_default', t.bool_node(node.has_default))
 	to_object(obj, 'is_interface', t.bool_node(node.is_interface))
 	to_object(obj, 'interface_type', t.type_node(node.interface_type))
+	expr_array := create_array()
+	for e in node.expr_types {
+		to_array(expr_array, t.type_node(e))
+	}
+	to_object(obj, 'expr_types', expr_array)
 	to_object(obj, 'pos', t.position(node.pos))
 	return obj
 }
