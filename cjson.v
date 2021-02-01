b173702ad12c94b30be6a21b3809105419b7c4e7
lydiandy <lydiandylin@gmail.com>
2021-02-02 01:42:48 +0800

    clean code

--- cjson.v
+++ cjson.v
@@ -24,38 +24,36 @@ fn C.cJSON_CreateNull() &C.cJSON
 
 // fn C.cJSON_CreateNumber() &C.cJSON
 // fn C.cJSON_CreateString() &C.cJSON
-fn C.cJSON_CreateRaw(arg_1 byteptr) &C.cJSON
+fn C.cJSON_CreateRaw(byteptr) &C.cJSON
 
-fn C.cJSON_IsInvalid(arg_1 voidptr) bool
+fn C.cJSON_IsInvalid(voidptr) bool
 
-fn C.cJSON_IsFalse(arg_1 voidptr) bool
+fn C.cJSON_IsFalse(voidptr) bool
 
 // fn C.cJSON_IsTrue(voidptr) bool
-fn C.cJSON_IsBool(arg_1 voidptr) bool
+fn C.cJSON_IsBool(voidptr) bool
 
-fn C.cJSON_IsNull(arg_1 voidptr) bool
+fn C.cJSON_IsNull(voidptr) bool
 
-fn C.cJSON_IsNumber(arg_1 voidptr) bool
+fn C.cJSON_IsNumber(voidptr) bool
 
-fn C.cJSON_IsString(arg_1 voidptr) bool
+fn C.cJSON_IsString(voidptr) bool
 
-fn C.cJSON_IsArray(arg_1 voidptr) bool
+fn C.cJSON_IsArray(voidptr) bool
 
-fn C.cJSON_IsObject(arg_1 voidptr) bool
+fn C.cJSON_IsObject(voidptr) bool
 
-fn C.cJSON_IsRaw(arg_1 voidptr) bool
+fn C.cJSON_IsRaw(voidptr) bool
 
-fn C.cJSON_AddItemToObject(arg_1 voidptr, arg_2 byteptr, arg_3 voidptr)
+fn C.cJSON_AddItemToObject(voidptr, byteptr, voidptr)
 
-fn C.cJSON_AddItemToArray(arg_1 voidptr, arg_2 voidptr)
+fn C.cJSON_AddItemToArray(voidptr, voidptr)
 
-fn C.cJSON_Delete(arg_1 voidptr)
+fn C.cJSON_Delete(voidptr)
 
-// fn C.cJSON_Parse() &C.cJSON
-fn C.cJSON_Print() byteptr
+fn C.cJSON_Print(voidptr) byteptr
 
-// fn C.cJSON_PrintUnformatted() byteptr
-// [inline]
+[inline]
 fn create_object() &C.cJSON {
 	return C.cJSON_CreateObject()
 }
@@ -113,6 +111,4 @@ fn add_item_to_array(obj &C.cJSON, item &C.cJSON) {
 fn json_print(json &C.cJSON) string {
 	s := C.cJSON_Print(json)
 	return tos3(s)
-	// s := C.cJSON_PrintUnformatted(json)
-	// return unsafe { tos(s, C.strlen(s)) }
 }
